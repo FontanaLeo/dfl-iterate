454b7f510b915850cb3ba70f67aeca3003b724bc
Leonardo Fontana <lerfontan@gmail.com>
2026-04-23 13:35:28 -0300

    refactor: remove unused exports from ParsonsProblem index file

--- src/components/atoms/ParsonsProblem/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './ParsonsProblemSourceBlock';
-export * from './ParsonsProblemTargetPlaceholder';
-export * from './ParsonsProblemSourceList';
-export * from './ParsonsProblemTargetList';
-export * from './ParsonsProblemAssembledCode';
-export * from './ParsonsProblemResultBanner';
-export * from './ParsonsProblemStatusInfo';
